f7b13eded6eb1e9e3283ae1ea1db692c32338057
LucasRT-coder <202504410562@alunos.estacio.br>
2025-05-22 20:08:09 +0000

    Fase novato final

--- SuperTrunfo_Desafio.c
+++ SuperTrunfo_Desafio.c
@@ -18,54 +18,62 @@ int main (){
 
      // Abertura do mesmo 
 printf("Desafio Aula: Super_Trunfo_C_Estacio. \n"); 
+printf("\n");
 
      // Inserir os dados da PRIMEIRA*1 carta  
-printf("Cadastro, digite o código da primeira cidade: \n");
+printf("Cadastro, digite o código da primeira cidade:");
 scanf("%s", codigo); 
 
-printf("Digite a área desta cidade: \n"); 
+printf("Digite a área desta cidade:"); 
 scanf("%f", &area); 
 
-printf("Digite a população: \n");
+printf("Digite a população:");
 scanf("%d", &populacao);
 
-printf("Digite o PIB: \n");
+printf("Digite o PIB:");
 scanf("%f", &pib);
 
-printf("Digite o número de pontos turísticos: \n");
+printf("Digite o número de pontos turísticos:");
 scanf("%d", &pontos_turisticos);
 
-     // Inserir os dados da SEGUNDA*2 carta  
-printf("Cadastro, digite o código da segunda cidade: \n");
-scanf("%s", &codigo2); 
+printf("\n");
 
-printf("Digite a área desta cidade: \n"); 
+     // Inserir os dados da SEGUNDA*2 carta  
+printf("Cadastro, digite o código da segunda cidade:");
+scanf("%s", codigo2); 
+
+printf("Digite a área desta cidade: "); 
 scanf("%f", &area2); 
 
-printf("Digite a população: \n");
+printf("Digite a população: ");
 scanf("%d", &populacao2);
 
-printf("Digite o PIB: \n");
+printf("Digite o PIB: ");
 scanf("%f", &pib2);
 
-printf("Digite o número de pontos turísticos: \n");
+printf("Digite o número de pontos turísticos: ");
 scanf("%d", &pontos_turisticos2);
 
+printf("\n");
+printf("\n");
+
      //Apresentação dos dados da PRIMEIRA*1 carta: 
-printf("Dados inseridos da Primeira Carta: \n"); 
+printf("Dados inseridos da Primeira Carta\n "); 
 printf("Código: %s\n", codigo); 
-printf("Àrea: %f", area); 
-printf("População: %d", populacao);
-printf("PIB: %f", pib); 
-printf("Pontos Turísticos: %d", pontos_turisticos); 
+printf("Àrea: %.2f km*2\n", area); 
+printf("População: %d\n milhões\n", populacao);
+printf("PIB: %.2f bilhões de reais\n", pib); 
+printf("Pontos Turísticos: %d\n", pontos_turisticos); 
+
+printf("\n");
 
      //Apresentação dos dados da SEGUNDA*2 carta:
-printf("Dados inseridos da Segunda Carta: \n"); 
+printf("Dados inseridos da Segunda Carta\n"); 
 printf("Código: %s\n", codigo2); 
-printf("Àrea: %f", area2); 
-printf("População: %d", populacao2);
-printf("PIB: %f", pib2); 
-printf("Pontos Turísticos: %d", pontos_turisticos2); 
+printf("Àrea: %.2f km*2\n", area2); 
+printf("População: %d\n milhões\n", populacao2);
+printf("PIB: %.2f bilhões de reais\n", pib2); 
+printf("Pontos Turísticos: %d\n", pontos_turisticos2); 
 
 
 
